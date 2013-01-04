614ee23e8560b4673110f5b5ad1d40c33214a1dc
Alex Hodgkins <ah19@sanger.ac.uk>
2013-01-04 09:31:56 +0000

    Bugfix: KillQCFarmJobs was not creating a kill_and_notify.err, so when attempting to view the log through the web interface an exception was generated.

--- lib/HTGT/QC/Util/KillQCFarmJobs.pm
+++ lib/HTGT/QC/Util/KillQCFarmJobs.pm
@@ -27,12 +27,15 @@ sub kill_unfinished_farm_jobs {
 
     my $self = shift;
 
-    my $outfile = $self->config->basedir->subdir($self->qc_run_id)->subdir("output")->file('kill_and_notify.out');
+    my $base = $self->config->basedir->subdir( $self->qc_run_id );
+    my $out_file = $base->subdir( "output" )->file( 'kill_and_notify.out' );
+    my $err_file = $base->subdir( "error" )->file( 'kill_and_notify.err' );
 
     run_cmd(
         'bsub',
         '-G', 'team87-grp',
-        '-o', $outfile,
+        '-o', $out_file,
+        '-e', $err_file,
         '-M', '500000', #we were running out of memory for some reason
         '-R', '"select[mem>500] rusage[mem=500]"',
         'qc kill-and-notify',
