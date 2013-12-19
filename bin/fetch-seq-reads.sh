46da6336271d81fc03470bce15fa10bf2e893b4e
af11_sanger <af11@sanger.ac.uk>
2013-12-19 11:09:44 +0000

    use environment perl. use correct oracle profile for htgt2 and farm3

--- bin/fetch-seq-reads.sh
+++ bin/fetch-seq-reads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -z "$DONE_RESET_ENV"; then
-    exec env -i DONE_RESET_ENV=yes $0 "$@"
+    exec env -i DONE_RESET_ENV=yes LSB_EXEC_CLUSTER=$LSB_EXEC_CLUSTER $0 "$@"
 fi
  
 function die () {
@@ -13,19 +13,23 @@ test -f /software/badger/etc/profile.badger || die "TraceServer access not avail
 source /software/badger/etc/profile.badger
 
 # The Oracle profile is in different places on 32- and 64-bit servers
-if test -f /software/oracle_instant_client_10_2/etc/profile.oracle; then
+if [[ $HOSTNAME =~ htgt2 ]] ; then
+    source /software/oracle-ic-11.2/etc/profile.oracle
+elif [[ $LSB_EXEC_CLUSTER =~ farm3 ]] ; then
+    source /software/oracle-ic-11.2/etc/profile.oracle
+elif test -f /software/oracle_instant_client_10_2/etc/profile.oracle; then
     source /software/oracle_instant_client_10_2/etc/profile.oracle
 elif test -f /software/oracle_instant_client_10_2/profile.oracle; then
     source /software/oracle_instant_client_10_2/profile.oracle
 else
-    die "Oracle instant client 10.2 not availale on this system"
+    die "Oracle instant client not availale on this system"
 fi
 
 test -n "$1" || die "TraceServer project must be specified"
 TRACE_PROJECT="$1"; shift
 
 if test "$1" = "--list-only"; then
-    /software/bin/perl /software/badger/bin/indir "${TRACE_PROJECT}"
+    perl /software/badger/bin/indir "${TRACE_PROJECT}"
 else
-    /software/bin/perl /software/badger/bin/indir "${TRACE_PROJECT}" | /software/badger/bin/exp-piece -fofn - left right
+    perl /software/badger/bin/indir "${TRACE_PROJECT}" | /software/badger/bin/exp-piece -fofn - left right
 fi
