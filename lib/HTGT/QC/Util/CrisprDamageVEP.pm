0079aba3d52fe9bb687d55ff98dfaf63336630c6
Sajith Perera <sajith.perera@gmail.com>
2014-06-25 09:18:53 +0100

    start using custom MutantProteinSeqs VEP plugin, store resultant fasta files

--- lib/HTGT/QC/Util/CrisprDamageVEP.pm
+++ lib/HTGT/QC/Util/CrisprDamageVEP.pm
@@ -97,6 +97,7 @@ has sam_file => (
 has [
     'bam_file', 'filtered_bam_file', 'bcf_file',      'pileup_file',
     'vcf_file', 'vep_file',          'vep_html_file', 'vcf_file_target_region',
+    'ref_protein_seq', 'mut_protein_seq',
     ] => (
     is  => 'rw',
     isa => 'Path::Class::File',
@@ -468,6 +469,7 @@ sub variant_effect_predictor {
         '--per_gene',                                   # Output the most severe consequence per gene
         '--symbol',                                     # Output gene symbol
         '--canonical',                                  # Mark if transcript is canonical
+        '--plugin', 'HTGT::QC::VEPPlugin::MutantProteinSeqs', # Use custom plugin to create protein sequence files
     );
 
     $self->log->debug( "vep command: " . join( ' ', @vep_command ) );
@@ -478,6 +480,12 @@ sub variant_effect_predictor {
 
     $self->vep_file( $vep_output );
     $self->vep_html_file( $self->dir->file('variant_effect_output.txt_summary.html')->absolute );
+
+    my $ref_seq_file = $self->dir->file('reference.fa')->absolute;
+    my $mut_seq_file = $self->dir->file('mutated.fa')->absolute;
+    $self->ref_protein_seq( $ref_seq_file ) if $self->dir->contains( $ref_seq_file );
+    $self->mut_protein_seq( $mut_seq_file ) if $self->dir->contains( $mut_seq_file );
+
     return;
 }
 
