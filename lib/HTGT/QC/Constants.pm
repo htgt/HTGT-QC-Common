97a4a06f8e29c641aa66a1945f6a8c34aa15f0d3
Sajith Perera <sajith.perera@gmail.com>
2015-01-19 08:33:03 +0000

    switch from scratch110 to scratch109

--- lib/HTGT/QC/Constants.pm
+++ lib/HTGT/QC/Constants.pm
@@ -28,14 +28,14 @@ const our $BCFTOOLS_CMD => $ENV{BCFTOOLS_CMD}
 const our $VEP_CMD => $ENV{VEP_CMD}
     // '/opt/t87/global/software/ensembl-tools-release-78/scripts/variant_effect_predictor/variant_effect_predictor.pl';
 const our $VEP_CACHE_DIR => $ENV{VEP_CACHE_DIR}
-    #// '/lustre/scratch109/blastdb/Ensembl/vep';
-    // '/lustre/scratch110/sanger/sp12/vep_cache';
+    // '/lustre/scratch109/blastdb/Ensembl/vep';
+    #// '/lustre/scratch110/sanger/sp12/vep_cache';
 
 const our %BWA_REF_GENOMES => (
-    #human => '/lustre/scratch109/blastdb/Users/team87/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa',
-    #mouse => '/lustre/scratch109/blastdb/Users/team87/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa',
-    human => '/lustre/scratch110/sanger/sp12/temp_ref_files/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa',
-    mouse => '/lustre/scratch110/sanger/sp12/temp_ref_files/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa',
+    human => '/lustre/scratch109/blastdb/Users/team87/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa',
+    mouse => '/lustre/scratch109/blastdb/Users/team87/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa',
+    #human => '/lustre/scratch110/sanger/sp12/temp_ref_files/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa',
+    #mouse => '/lustre/scratch110/sanger/sp12/temp_ref_files/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa',
 );
 
 const our %CURRENT_ASSEMBLY => (
