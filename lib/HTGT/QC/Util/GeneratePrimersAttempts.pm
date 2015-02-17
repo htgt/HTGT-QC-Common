98b330212abf567159b08719b3eac0cd4800ab6e
af11_sanger <af11@sanger.ac.uk>
2015-02-17 09:57:26 +0000

    remove unnecessary primer config option

--- lib/HTGT/QC/Util/GeneratePrimersAttempts.pm
+++ lib/HTGT/QC/Util/GeneratePrimersAttempts.pm
@@ -191,17 +191,6 @@ has check_genomic_specificity => (
     default => 1,
 );
 
-# Some additional factors to allow us to adjust the product size range
-# to replicate existing primer generation code
-
-# Set this to true if you want the minimun product size to be:
-# length of target region + five prime offset + three prime offset
-has product_contains_region_offsets => (
-    is      => 'ro',
-    isa     => 'Bool',
-    default => 0,
-);
-
 # By default the maximum product length is:
 # length of target + five prime region + three prime region
 # Use this attribute if you want the max product to be <n> bases shorter than this
@@ -407,13 +396,6 @@ sub generate_primer_product_size_range {
         }
         if ( $self->current_attempt == 1 ) {
             $min_size = $self->target_end - $self->target_start;
-            ## FIXME: added this to get the same params as in existing code
-            # but I think it is not necessary as primer product will always
-            # contain the region offsets because the pair must flank the region
-            # of interest plus the offsets
-            if($self->product_contains_region_offsets){
-                $min_size += $self->three_prime_region_offset + $self->five_prime_region_offset;
-            }
         }
         else {
             my $last_size_range = $self->product_size_array->[0];
