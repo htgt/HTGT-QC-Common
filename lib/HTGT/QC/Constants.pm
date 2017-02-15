40df69a3b2c05b8d49512893a22b878e7de69fd6
Tiago Grego <tg6@sanger.ac.uk>
2017-02-15 15:32:08 +0000

    lib/HTGT/QC/Constants.pm switch to scratch117

--- lib/HTGT/QC/Constants.pm
+++ lib/HTGT/QC/Constants.pm
@@ -19,22 +19,23 @@ BEGIN {
     our %EXPORT_TAGS = ();
 }
 
-const our $BWA_MEM_CMD  => $ENV{BWA_MEM_CMD}
-    // '/software/vertres/bin-external/bwa-0.7.5a-r406/bwa';
-const our $SAMTOOLS_CMD => $ENV{SAMTOOLS_CMD}
-    // '/software/vertres/bin-external/samtools-0.2.0-rc8/bin/samtools';
-const our $BCFTOOLS_CMD => $ENV{BCFTOOLS_CMD}
-    // '/software/vertres/bin-external/samtools-0.2.0-rc8/bin/bcftools';
-const our $VEP_CMD => $ENV{VEP_CMD}
-    // '/opt/t87/global/software/ensembl-tools-release-80/scripts/variant_effect_predictor/variant_effect_predictor.pl';
-const our $VEP_CACHE_DIR => $ENV{VEP_CACHE_DIR}
-    // '/lustre/scratch109/blastdb/Ensembl/vep';
+const our $BWA_MEM_CMD  => $ENV{BWA_MEM_CMD} //
+    '/software/vertres/bin-external/bwa-0.7.5a-r406/bwa';
+const our $SAMTOOLS_CMD => $ENV{SAMTOOLS_CMD} //
+    '/software/vertres/bin-external/samtools-0.2.0-rc8/bin/samtools';
+const our $BCFTOOLS_CMD => $ENV{BCFTOOLS_CMD} //
+    '/software/vertres/bin-external/samtools-0.2.0-rc8/bin/bcftools';
+const our $VEP_CMD => $ENV{VEP_CMD} //
+    '/opt/t87/global/software/ensembl-tools-release-80/scripts/variant_effect_predictor/variant_effect_predictor.pl';
+const our $VEP_CACHE_DIR => $ENV{VEP_CACHE_DIR} //
+    '/lustre/scratch117/core/corebio/blastdb/Ensembl/vep';
+#    '/lustre/scratch109/blastdb/Ensembl/vep';
 
 const our %BWA_REF_GENOMES => (
-    human => ( $ENV{BWA_REF_GENOME_HUMAN_FA}
-             // '/lustre/scratch109/blastdb/Users/team87/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa'),
-    mouse => ( $ENV{BWA_REF_GENOME_MOUSE_FA}
-             // '/lustre/scratch109/blastdb/Users/team87/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa'),
+    human => ( $ENV{BWA_REF_GENOME_HUMAN_FA} //
+        '/lustre/scratch117/sciops/team87/blastdb/Human/bwa/Homo_sapiens.GRCh38.dna.primary_assembly.clean_chr_names.fa'),
+    mouse => ( $ENV{BWA_REF_GENOME_MOUSE_FA} //
+        '/lustre/scratch117/sciops/team87/blastdb/Mouse/bwa/Mus_musculus.GRCm38.toplevel.clean_chr_names.fa'),
 );
 
 const our %CURRENT_ASSEMBLY => (
